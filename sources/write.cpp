23f1b4eef4a4d9119d739dc03050f9c2a750255f
Matis Codjia <codjiamatis01@gmail.com>
2024-08-07 22:55:24 +0200

    No major learning

--- sources/write.cpp
+++ sources/write.cpp
@@ -5,7 +5,7 @@ int main()
     {
         std::ofstream fichier {"sortie.txt", std::ios::app};
 
-        fichier << 3 << " " << 4;
+        fichier << 3 << ", les chiffres " << 4; 
 
         return 0;
     }
